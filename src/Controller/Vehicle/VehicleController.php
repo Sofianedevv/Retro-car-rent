649cd117b37c3cf6f0f5980611d295c60ff0c5ca
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-12-05 21:57:33 +0100

    add vehicle controller

---
+++ src/Controller/Vehicle/VehicleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class VehicleController extends AbstractController
+{
+    #[Route('/nos-vehicules', name: 'vehicle_collections')]
+    public function collection(): Response
+    {
+        return $this->render('vehicle/collections.html.twig');
+    }
+}
