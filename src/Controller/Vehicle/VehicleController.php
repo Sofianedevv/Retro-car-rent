b091f0288e439595d854d0619e73f3442baebe04
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-12-26 20:29:56 +0100

    add a vehicle in favorite

--- src/Controller/Vehicle/VehicleController.php
+++ src/Controller/Vehicle/VehicleController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\VehicleRepository;
 use App\Entity\Car;
+use App\Entity\Favorite;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class VehicleController extends AbstractController
 {
@@ -23,28 +26,74 @@ class VehicleController extends AbstractController
         ]);
     }
     #[Route('/nos-voitures', name: 'app_car')]
-    public function show_cars(VehicleRepository $vehicleRepository): Response
+    public function show_cars(VehicleRepository $vehicleRepository, EntityManagerInterface $entityManager): Response
     {
         $cars = $vehicleRepository->findAllCars();
-        return $this->render('vehicle/_display_car.html.twig', [
-            'cars' => $cars
+        
+        $isFavorite = [];
+        
+        $user = $this->getUser();
+        if ($user) {
+        
+            $favorite = $entityManager->getRepository(Favorite::class)->findOneBy(['client' => $user]);
+            if ($favorite) {
+                $favoriteVehicles = $favorite->getVehicles();
+                foreach ($cars as $car) {
+                    $isFavorite[$car->getId()] = $favoriteVehicles->contains($car);
+                }
+            }
+        }
+            return $this->render('vehicle/_display_car.html.twig', [
+            'cars' => $cars,
+            'isFavorite' => $isFavorite
         ]);
     }
     #[Route('/nos-motos', name: 'app_motorcycle')]
-    public function show_motorcycle(VehicleRepository $vehicleRepository): Response
+    public function show_motorcycle(VehicleRepository $vehicleRepository, EntityManagerInterface $entityManager): Response
     {
         $motorcycles = $vehicleRepository->findAllMotorcycles();
-     
+    
+        $isFavorite = [];
+        
+        $user = $this->getUser();
+        if ($user) {
+            $favorite = $entityManager->getRepository(Favorite::class)->findOneBy(['client' => $user]);
+            if ($favorite) {
+                $favoriteVehicles = $favorite->getVehicles();
+                foreach ($motorcycles as $motorcycle) {
+                    $isFavorite[$motorcycle->getId()] = $favoriteVehicles->contains($motorcycle);
+                }
+            }
+        }
+    
         return $this->render('vehicle/_display_motorcycle.html.twig', [
-            'motorcycles' => $motorcycles
+            'motorcycles' => $motorcycles,
+            'isFavorite' => $isFavorite
         ]);
     }
+    
     #[Route('/nos-van', name: 'app_van')]
-    public function show_vans(VehicleRepository $vehicleRepository): Response
+    public function show_vans(VehicleRepository $vehicleRepository, EntityManagerInterface $entityManager): Response
     {
         $vans = $vehicleRepository->findAllVan();
+    
+        $isFavorite = [];
+        
+        $user = $this->getUser();
+        if ($user) {
+            $favorite = $entityManager->getRepository(Favorite::class)->findOneBy(['client' => $user]);
+            if ($favorite) {
+                $favoriteVehicles = $favorite->getVehicles();
+                foreach ($vans as $van) {
+                    $isFavorite[$van->getId()] = $favoriteVehicles->contains($van);
+                }
+            }
+        }
+    
         return $this->render('vehicle/_display_van.html.twig', [
             'vans' => $vans,
+            'isFavorite' => $isFavorite,
         ]);
     }
+    
 }
