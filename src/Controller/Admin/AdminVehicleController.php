e52782606dde483094abb862b5051e26c12cff9a
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2025-01-05 20:48:05 +0100

    gestion image multiple moto et van

--- src/Controller/Admin/AdminVehicleController.php
+++ src/Controller/Admin/AdminVehicleController.php
@@ -20,6 +20,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Filesystem\Filesystem;
 
 #[Route('/admin')]
 class AdminVehicleController extends AbstractController
@@ -127,14 +128,39 @@ class AdminVehicleController extends AbstractController
 
     // Routes pour l'édition
     #[Route('/vehicle/car/{id}/edit', name: 'admin_vehicle_car_edit')]
-    public function editCar(Request $request, Car $car, EntityManagerInterface $entityManager): Response
-    {
+    public function editCar(
+        Request $request, 
+        Car $car, 
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
         $form = $this->createForm(CarType::class, $car);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            /** @var UploadedFile[] $imageFiles */
+            $imageFiles = $form->get('imageFiles')->getData();
 
+            foreach ($imageFiles as $imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('vehicle_images_directory'),
+                        $newFilename
+                    );
+
+                    $vehicleImage = new VehicleImage();
+                    $vehicleImage->setUrl($newFilename);
+                    $car->addVehicleImage($vehicleImage);
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Une erreur est survenue lors du téléchargement de l\'image');
+                }
+            }
+
+            $entityManager->flush();
             $this->addFlash('success', 'La voiture a été modifiée avec succès.');
             return $this->redirectToRoute('admin_vehicles');
         }
@@ -144,6 +170,126 @@ class AdminVehicleController extends AbstractController
             'vehicle' => $car,
             'vehicle_type' => 'car'
         ]);
+    }
+
+    #[Route('/vehicle/van/{id}/edit', name: 'admin_vehicle_van_edit')]
+    public function editVan(
+        Request $request, 
+        Van $van, 
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
+        $form = $this->createForm(VanType::class, $van);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile[] $imageFiles */
+            $imageFiles = $form->get('imageFiles')->getData();
+
+            foreach ($imageFiles as $imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('vehicle_images_directory'),
+                        $newFilename
+                    );
+
+                    $vehicleImage = new VehicleImage();
+                    $vehicleImage->setUrl($newFilename);
+                    $van->addVehicleImage($vehicleImage);
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Une erreur est survenue lors du téléchargement de l\'image');
+                }
+            }
+
+            $entityManager->flush();
+            $this->addFlash('success', 'Le van a été modifié avec succès.');
+            return $this->redirectToRoute('admin_vehicles');
+        }
+
+        return $this->render('admin/vehicles/edit.html.twig', [
+            'form' => $form->createView(),
+            'vehicle' => $van,
+            'vehicle_type' => 'van'
+        ]);
+    }
+
+    #[Route('/vehicle/motorcycle/{id}/edit', name: 'admin_vehicle_motorcycle_edit')]
+    public function editMotorcycle(
+        Request $request, 
+        Motorcycle $motorcycle, 
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
+        $form = $this->createForm(MotorcycleType::class, $motorcycle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile[] $imageFiles */
+            $imageFiles = $form->get('imageFiles')->getData();
+
+            foreach ($imageFiles as $imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('vehicle_images_directory'),
+                        $newFilename
+                    );
+
+                    $vehicleImage = new VehicleImage();
+                    $vehicleImage->setUrl($newFilename);
+                    $motorcycle->addVehicleImage($vehicleImage);
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Une erreur est survenue lors du téléchargement de l\'image');
+                }
+            }
+
+            $entityManager->flush();
+            $this->addFlash('success', 'La moto a été modifiée avec succès.');
+            return $this->redirectToRoute('admin_vehicles');
+        }
+
+        return $this->render('admin/vehicles/edit.html.twig', [
+            'form' => $form->createView(),
+            'vehicle' => $motorcycle,
+            'vehicle_type' => 'motorcycle'
+        ]);
+    }
+
+    #[Route('/vehicle/image/{id}/delete', name: 'admin_vehicle_image_delete')]
+    public function deleteImage(
+        VehicleImage $image, 
+        EntityManagerInterface $entityManager,
+        Filesystem $filesystem
+    ): Response {
+        $vehicle = $image->getVehicle();
+        $vehicleType = match (true) {
+            $vehicle instanceof Car => 'car',
+            $vehicle instanceof Van => 'van',
+            $vehicle instanceof Motorcycle => 'motorcycle',
+            default => throw new \RuntimeException('Type de véhicule inconnu'),
+        };
+        
+        // Supprimer le fichier physique
+        $imagePath = $this->getParameter('vehicle_images_directory').'/'.$image->getUrl();
+        if ($filesystem->exists($imagePath)) {
+            $filesystem->remove($imagePath);
+        }
+
+        // Supprimer l'entrée en base de données
+        $entityManager->remove($image);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'L\'image a été supprimée avec succès.');
+        
+        // Rediriger vers la page d'édition appropriée
+        return $this->redirectToRoute("admin_vehicle_{$vehicleType}_edit", ['id' => $vehicle->getId()]);
     }
 
     // Ajoutez les méthodes similaires pour l'édition des vans et des motos
--- src/Form/MotorcycleType.php
+++ src/Form/MotorcycleType.php
@@ -11,8 +11,11 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Image;
 
 class MotorcycleType extends AbstractType
 {
@@ -101,6 +104,30 @@ class MotorcycleType extends AbstractType
                 ],
                 'label_attr' => [
                     'class' => 'text-sm font-medium text-gray-700'
+                ]
+            ])
+            ->add('imageFiles', FileType::class, [
+                'label' => 'Photos du véhicule',
+                'multiple' => true,
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'accept' => 'image/*',
+                    'class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
+                ],
+                'constraints' => [
+                    new All([
+                        'constraints' => [
+                            new Image([
+                                'maxSize' => '5M',
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/png',
+                                ],
+                                'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPG ou PNG)',
+                            ])
+                        ]
+                    ])
                 ]
             ]);
     }
