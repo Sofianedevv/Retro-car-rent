1c8061d723544c8c337ef56c7ea7dea7d36d8433
Lydia-BEDRI <bedrilydia118@gmail.com>
2025-01-11 21:56:38 +0100

    add style/design to the header

--- public/template-html/tailwind.config.js
+++ public/template-html/tailwind.config.js
@@ -1,41 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,twig}"],
-  theme: {
-    container: {
-      center: true,
-      padding: '1rem',
-  },
-  screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-  },
-  fontFamily: {
-     manrope: ['Mangrove', 'sans-serif'],
-  },
-  colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: '#fdfcf3', // Blanc cassé pour un effet vintage
-      black: '#3b302f', // Noir doux pour les contrastes
-      primary: '#8B4513', // Orange rétro
-      secondary: '#6b705c', // Vert olive doux
-      accent: '#cb997e', // Beige rosé pour des touches subtiles
-      gray: {
-          lighter: '#ede0d4', // Beige clair
-          light: '#a5a58d', // Gris olive
-          dark: 'rgba(107,112,92,0.83)', // Vert gris
-          txt: '#4f4a41', // Brun textuel doux
-          line: '#d6ccc2', // Beige clair pour les séparateurs
-      },
-  },
-    extend: {
-      colors: {
-        primary: '#8B4513',
-      },
+    content: ["./**/*.{html,js,twig}" ], // inclut tous les fichiers twig et autres fichiers HTML/JS],
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
+        fontFamily: {
+            'playfair': ['Playfair Display', 'serif'],
+        },
+        extend: {
+            colors: {
+                'primary': '#e0d8b0'
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
