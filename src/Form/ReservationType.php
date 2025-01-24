0a5065212e352ebd35d44759c12d51b267ee6aa2
Lydia-BEDRI <bedrilydia118@gmail.com>
2025-01-25 00:32:46 +0100

    add style/design to recap reservation

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -2,45 +2,43 @@
 
 namespace App\Form;
 
-
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReservationType extends AbstractType
 {
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-
         $builder
-        ->add('startDate', DateTimeType::class, [
-            'label' => 'Date de location',
-            'attr' => [
-                'placeholder' => 'Choisissez une date',
-                'class'=>'form-input rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 w-full text-black',
-                
-            ],
-             'input'  => 'datetime_immutable'
-        ])
-        ->add('endDate', DateTimeType::class, [
-            'label' => 'Date de location',
-            'attr' => [
-                'placeholder' => 'Choisissez une date',
-               
-                'input'  => 'datetime_immutable',
-                'class'=>'form-input rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 w-full text-black',
-            ],
-        ]);
-        
+            ->add('startDate', DateType::class, [
+                'label' => 'Date de départ',
+                'widget' => 'single_text',
+            ])
+            ->add('startTime', TimeType::class, [
+                'label' => 'Heure de départ',
+                'widget' => 'single_text',
+                'html5' => true,
+                'attr' => ['class' => 'time-interval'],
+            ])
+            ->add('endDate', DateType::class, [
+                'label' => 'Date de retour',
+                'widget' => 'single_text',
+            ])
+            ->add('endTime', TimeType::class, [
+                'label' => 'Heure de retour',
+                'widget' => 'single_text',
+                'html5' => true,
+                'attr' => ['class' => 'time-interval'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => null,
-            'vehicle' => [],
         ]);
     }
 }
